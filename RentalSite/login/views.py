48964cea07a97d92f3fe1ad86a1ec548cedd3de4
huifeng-kooboo <gisdoing@gmail.com>
2019-05-26 15:07:05 +0800

    修复表单中查询数据的bug
    将django数据库get方法，改成filter 保证传入数据时候不报错

--- RentalSite/login/views.py
+++ RentalSite/login/views.py
@@ -11,14 +11,14 @@ def login(request):
         if ret: #表单有效时候
             username = request.POST.get("username")
             password = request.POST.get("password")
-            record = UserLogin.objects.get(username=username,password=password) #获取数据库数据
-            if record.DoesNotExist: #没找到数据
+            record = UserLogin.objects.filter(username=username,password=password) #获取数据库数据
+            if len(record) < 1: #没找到数据
                 warn_login_str = "用户名或密码错误"
                 return render(request,"login/login.html",{"login_other_error":warn_login_str}) #返回用户名或密码错误信息
             else:
                 return render(request,"main/main.html") #登录成功，进入主界面
         else:
-            error = LoginForm.errors
+            error = objPOST.errors
             return render(request,"login/login.html",{"login_error":error}) #返回验证有效错误信息
     return render(request,"login/login.html") #登录界面
 
@@ -32,18 +32,16 @@ def register(request):
             password = request.POST.get("password")
             idcard = request.POST.get("idcard")
             rentaddress = request.POST.get("rentaddress")
-            record = UserRegister.objects.get(username=username) #判断是否已经注册账号
-            if record.DoesNotExist:#当没注册时：添加账号信息
-                obj = UserRegister(username=username,password=password,idcard=idcard,rentaddress=rentaddress)
-                obj_login = UserLogin(username=username,password=password) #保存一份到登录数据库
-                obj.save() #添加数据到注册数据库
-                obj_login.save() #同步数据到登录数据库
+            record = UserRegister.objects.filter(username=username) #判断是否已经注册账号
+            if len(record) < 1:#当没注册时：添加账号信息
+                obj = UserRegister.objects.create(username=username,password=password,idcard=idcard,rentaddress=rentaddress)
+                obj_login = UserLogin.objects.create(username=username,password=password) #保存一份到登录数据库
                 return render(request,"login/login.html") #注册成功，进入登录界面
             else:
                 warn_str = "当前用户已注册" #返回当前用户已注册信息到前端
-                return render(request,"login/register.html",{"warn_login":warn_str}) #返回当前账号已注册信息到前端界面
+                return render(request,"login/register.html",{"warn_login":warn_str}) #返回当前账号已注册信息到前端界面,用js弹窗显示
         else:
-            error = RegisterForm.errors
+            error = objPOST.errors
             return render(request,"login/register.html",{"register_error":error}) #返回注册信息有误：根据表单验证情况
     return render(request,"login/register.html") #在没有POST请求时，进入注册主界面
 
