51cef48a1997a3759c6ebc409c46020833c3be63
huifeng-kooboo <gisdoing@gmail.com>
2019-06-30 06:08:46 +0800

    删除vs部分
    删除vs部分，使得更加方便，简洁

--- RentalSite/login/admin.py
+++ RentalSite/login/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from.models import UserLogin,UserRegister,RentalInfo,RentHouseInfo,LandloadInfo
 
-#注册模型到后台，可以直接在后台进行增删改查
+#注册模型到后台，可以直接在admin后台进行增删改查
 admin.site.register(UserLogin)
 admin.site.register(UserRegister)
 admin.site.register(RentHouseInfo)
