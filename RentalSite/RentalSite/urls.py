cab377046883a75f5df2f3a2dc7c7f1ebfafe758
huifeng-kooboo <gisdoing@gmail.com>
2019-07-06 10:07:48 +0800

    修复image字段显示问题
    修复image属性显示到前端加载不出来的问题：
    解决方案：
    setting里面，添加url，用来定向到保存该文件的路径，
    然后前端显示的话，方法如下：
    <img src='{{house_list.house_image.url}}'/>

--- RentalSite/RentalSite/urls.py
+++ RentalSite/RentalSite/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls), #后台管理
-    path('login/',include('login.urls')) #登录、注册模块
+    path('login/',include('login.urls')) #主要模块入口，包括登录，主界面等等
 ]
+
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root = settings.MEDIA_ROOT)
